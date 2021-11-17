218618ffeab4dd5fdaaf0b8104160681fa655afc
faisaltmz007 <63185311+FaisalTmz007@users.noreply.github.com>
2021-11-17 09:27:05 +0700

    Add files via upload

---
+++ Tugas Mas Zidny.py
@@ -0,0 +1,7 @@
+nama = 'Muhamad Faisal Fachrul Ulum'
+nim = 212410102030
+prodi = 'Teknologi Informasi'
+univ = 'Universitas Jember'
+asal = 'Pasuruan'
+
+print(f'\nPerkenalkan saya {nama}\ndengan NIM {nim}\ndari prodi {prodi} {univ}\ndan saya berasal dari {asal}\n')
